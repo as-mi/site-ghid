5328651abd92d171bbec9f9c225b5fe6cdcd15f8
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-09-22 20:45:44 +0300

    Add HTML minification transform

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,23 @@
+const htmlmin = require("html-minifier");
 const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
-  // Add a filter using the Config API
+  // Add a filter for minimizing CSS
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  // Add a transform for minimizing HTML
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath.endsWith(".html")) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+    }
+
+    return content;
   });
 
   return {
