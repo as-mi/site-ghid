7975bad6c640133b01c87f330b0745c95f632fdf
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-09-25 22:18:08 +0300

    Elimină CleanCSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,6 @@
 const htmlmin = require("html-minifier");
-const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
-  // Add a filter for minimizing CSS
-  eleventyConfig.addFilter("cssmin", function (code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
-
   // Add a transform for minimizing HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath.endsWith(".html")) {
